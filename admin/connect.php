dd6008a0df49612694958a09088dec026c1ccce6
sasangihasadara <sasangihasadara@gmail.com>
2026-02-04 01:12:34 +0530

    change

--- admin/connect.php
+++ admin/connect.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "optical_shop";
-
+//create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
